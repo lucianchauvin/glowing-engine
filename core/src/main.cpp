e558c8f8767ddffe7943c2e911b2790e34d6e332
Brian Glasheen <pokemongamer223223@gmail.com>
2025-03-02 02:43:12 -0600

    mouse work

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -63,6 +63,8 @@ int main()
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
 
     // tell GLFW to capture our mouse
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
