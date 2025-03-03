525c8a982945fef448d73da8544ccaf276f77bfd
Brian Glasheen <pokemongamer223223@gmail.com>
2025-03-02 19:21:39 -0600

    gtfo git status

---
+++ build/CMakeFiles/CORE.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CORE.dir/ext/dearimgui/imgui.cpp.o"
+  "CMakeFiles/CORE.dir/ext/dearimgui/imgui.cpp.o.d"
+  "CMakeFiles/CORE.dir/ext/dearimgui/imgui_draw.cpp.o"
+  "CMakeFiles/CORE.dir/ext/dearimgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/CORE.dir/ext/dearimgui/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/CORE.dir/ext/dearimgui/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/CORE.dir/ext/dearimgui/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/CORE.dir/ext/dearimgui/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/CORE.dir/ext/dearimgui/imgui_tables.cpp.o"
+  "CMakeFiles/CORE.dir/ext/dearimgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/CORE.dir/ext/dearimgui/imgui_widgets.cpp.o"
+  "CMakeFiles/CORE.dir/ext/dearimgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/CORE.dir/ext/glad/glad.c.o"
+  "CMakeFiles/CORE.dir/ext/glad/glad.c.o.d"
+  "CMakeFiles/CORE.dir/src/main.cpp.o"
+  "CMakeFiles/CORE.dir/src/main.cpp.o.d"
+  "CORE"
+  "CORE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/CORE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
