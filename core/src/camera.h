6b74ce925df71db22f2bc390d3a3ef4716d5cc1d
BrianGlasheen <pokemongamer223223@gmail.com>
2025-03-03 22:49:59 -0600

    increase fov

--- core/src/camera.h
+++ core/src/camera.h
@@ -98,8 +98,8 @@ public:
         Zoom -= (float)yoffset;
         if (Zoom < 1.0f)
             Zoom = 1.0f;
-        if (Zoom > 45.0f)
-            Zoom = 45.0f;
+        if (Zoom > 360.0f)
+            Zoom = 360.0f;
     }
 
 private:
